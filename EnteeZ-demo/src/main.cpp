d6617502aa87379946ae11dc35d54188bdc6a2a9
John Green <jkgreen@uclan.ac.uk>
2019-03-22 13:30:29 +0000

    Updated demo project

--- EnteeZ-demo/src/main.cpp
+++ EnteeZ-demo/src/main.cpp
@@ -1,23 +1,14 @@
 #include <EnteeZ/EnteeZ.hpp>
 #include <iostream>
+#include <sstream>
 #include <vector>
-
 
 struct Position
 {
-	Position() {}
-	Position(float x, float y) : x(x), y(y) {}
+	Position(float x, float y, float z) : x(x), y(y), z(z) {}
 	float x;
 	float y;
-};
-
-
-class Collidable
-{
-public:
-	Collidable() {}
-	Collidable(float rad) :rad(rad) {}
-	float rad;
+	float z;
 };
 
 template<typename T>
@@ -29,60 +20,58 @@ private:
 
 };
 
-class MsgSend
+template<typename T>
+void SendMessage(enteez::Entity* target, enteez::Entity* sender, T data)
 {
-public:
-	MsgSend() {}
-	MsgSend(enteez::Entity* entity) : m_entity(entity) {}
-	template<typename T>
-	void Send(T data)
+	target->ForEach<MsgRecive<T>>([data](enteez::Entity* entity, MsgRecive<T>& recive)
 	{
-		m_entity->ForEach<MsgRecive<T>>([data](enteez::Entity* entity, MsgRecive<T>& recive)
+		recive.ReciveMessage(entity, data);
+	});
+}
+
+struct MoveMessage
+{
+	float x;
+	float y;
+	float z;
+};
+
+struct Logic
+{
+	virtual void Update() = 0;
+};
+
+struct PlayerMovment : public Logic, public MsgRecive<MoveMessage>
+{
+	PlayerMovment(enteez::Entity* entity) : m_entity(entity) {}
+
+	virtual void ReciveMessage(enteez::Entity* sender, MoveMessage message)
+	{
+		if (m_entity->HasComponent<Position>())
 		{
-			recive.ReciveMessage(entity, data);
-		});
+			Position& position = m_entity->GetComponent<Position>();
+			position.x += message.x;
+			position.y += message.y;
+			position.z += message.z;
+		}
+	}
+
+	virtual void Update()
+	{
+		if (m_entity->HasComponent<Position>())
+		{
+			Position& position = m_entity->GetComponent<Position>();
+			// Move the player forward
+			position.z += 10;
+		}
 	}
 private:
 	enteez::Entity* m_entity;
 };
 
-class ComponentA : public MsgSend
-{
-public:
-	ComponentA() {}
-	ComponentA(enteez::Entity* entity) : MsgSend(entity){}
-	void Update()
-	{
-		Send(123);
-	}
-private:
-
-};
-
-class ComponentB : public MsgSend, public MsgRecive<int>
-{
-public:
-	ComponentB() : MsgSend() {}
-	ComponentB(enteez::Entity* entity) : MsgSend(entity) {}
-	virtual void ReciveMessage(enteez::Entity* sender, int message)
-	{
-		std::cout << "B Wahoo " << message << std::endl;
-	}
-private:
-
-};
-
-struct A
-{
-	~A()
-	{
-		std::cout << "Called" << std::endl;
-	}
-};
-
 int main(int argc, char **argv)
 {
-
+	// Memory Leak Checking
 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
 	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
 	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
@@ -91,74 +80,147 @@ int main(int argc, char **argv)
 	_CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDOUT);
 
 	{
+		// Creation of entity manager
 		enteez::EnteeZ ez;
-
 		enteez::EntityManager& em = ez.GetEntityManager();
 
-
-		enteez::Entity* entity1 = em.CreateEntity();
-		enteez::ComponentWrapper<A>* comp = entity1->AddComponent<A>();
-		entity1->RemoveComponent(comp->GetComponentPtr());
-
-		ez.RegisterBase<ComponentA, MsgSend>();
-		ez.RegisterBase<ComponentB, MsgSend, MsgRecive<int>>();
-
-		enteez::Entity* entity2 = em.CreateEntity();
-		entity2->AddComponent<ComponentA>(entity2);
-		entity2->AddComponent<ComponentB>(entity2);
-
-
-
-		entity2->ForEach<MsgSend>([](enteez::Entity * entity, MsgSend & send)
+		// EG 1
 		{
-
-		});
-
-		Position* position = new Position(1.0f, 2.0f);
-
-		
-		enteez::Entity* entity = em.CreateEntity();
-		entity->AddComponent(position);
-		std::cout << entity->HasComponent<Position>() << std::endl;
-
-
-		em.ForEach<ComponentA, ComponentB>([](enteez::Entity * entity, ComponentA & caomponent_a, ComponentB & caomponent_b)
-		{
-
-		}, true);
-
-
-		for (unsigned int i = 0; i < 10; i++)
-		{
+			std::cout << "Example 1" << std::endl;
+			// Create a entity
 			enteez::Entity* entity = em.CreateEntity();
-			entity->AddComponent<Position>(1.2f, 1.3f);
-			ComponentA& compa = entity->AddComponent<ComponentA>(entity)->Get();
-			entity->AddComponent<ComponentB>();
-			compa.Update();
-			entity->RemoveComponent<ComponentA>();
+			// Attach a structure to the entity and initilize its constructor with 3 floats
+			entity->AddComponent<Position>(1.0f, 2.0f, 3.0f);
+			// Check to see if we have the component
+			bool hasComponent = entity->HasComponent<Position>();
+			std::cout << "Has Component: " << (hasComponent ? "True" : "False") << std::endl;
+			if (hasComponent)
+			{
+				// Get component and output its data
+				Position& position = entity->GetComponent<Position>();
+				std::cout << "Data: X:" << position.x << ", Y:" << position.y << ", Z:" << position.z << std::endl;
+			}
+			// Clean up the entity
+			entity->Destroy();
+			// or
+			//em.DestroyEntity(entity);
 		}
 
-		em.ForEach<ComponentA>([](enteez::Entity * entity, ComponentA & caomponent_a)
-			{
-				std::cout << "a" << std::endl;
-			}, true);
+		std::cout << std::endl;
 
-		for (auto& entity : em.GetEntitysWith<ComponentA>())
-		{
+		{ // EG 2: Component looping
+			std::cout << "Example 2" << std::endl;
+			// Create a pointer array for 10 new entitys
+			enteez::Entity** entities = new enteez::Entity*[10];
+			// Will be used to store entity names
+			std::stringstream ss;
+			// Initilize them
+			for (int i = 0; i < 10; i++)
+			{
+				// Create the entity
+				entities[i] = em.CreateEntity();
+				ss << "Entity " << i;
+				// Add a name to the entity
+				entities[i]->SetName(ss.str());
+				// Clear the name buffer
+				ss.str("");
+
+				// Only add a component to every other entity
+				if (i % 2 == 0)
+				{
+					entities[i]->AddComponent<Position>(1.0f, 2.0f, 3.0f);
+				}
+			}
+
+			// There is a optional setting in the for each loop to allow any results to be cached.
+			// Later on if a new entity is changed and it no longer or now matches the cache, the cache will automaticly update
+			bool shouldCache = true;
+
+			// Loop through all entitys and find one's that have x components. They must have all the components for it to be a match
+			em.ForEach<Position>([](enteez::Entity* entity, Position& position) {
+				std::cout << entity->GetName() << std::endl;
+			}, shouldCache);
+
+			// Clean up all entitys
+			for (auto& it : em.GetEntitys())
+			{
+				it->Destroy();
+			}
+			delete entities;
+		}
+
+		std::cout << std::endl;
+
+		{ // EG 3 : Looping through components that inherit another
+			std::cout << "Example 3" << std::endl;
+
+			// Since C++ dose not have any form of meta data that retains information about what inherts what else. We have to tell Enteez about this inheritance
+			// Here we are saying that the Player Movment component will inherit Logic
+			ez.RegisterBase<PlayerMovment, Logic>();
+			// You can also define things your inherited objects inherit if needed as well as define multiple inheritances at the same time
+			//ez.RegisterBase<PlayerMovment, Logic, .., ..>();
+
+			// Create a entity
+			enteez::Entity* entity = em.CreateEntity();
+
+			// Add 2 components, one with the inheritance and one without
+			entity->AddComponent<Position>(1.0f, 2.0f, 3.0f);
+			entity->AddComponent<PlayerMovment>(entity);
+
+			// Loop through the components for each component that inherits the requested object
+			entity->ForEach<Logic>([](enteez::Entity* entity, Logic& logic) {
+				logic.Update();
+			});
+
+			// There is no inbuilt safeguard for when a component dose not exist for effitency, so it is down to the user to make sure entitys exist before accessing
+			if (entity->HasComponent<Position>())
+			{
+				Position& position = entity->GetComponent<Position>();
+				std::cout << "Position: X:" << position.x << ", Y:" << position.y << ", Z:" << position.z << std::endl;
+			}
+
+			// Cleanup
 			entity->Destroy();
 		}
 
-		em.ForEach<ComponentA>([](enteez::Entity * entity, ComponentA & caomponent_a)
+		std::cout << std::endl;
+
+		{ // EG 4
+			std::cout << "Example 4" << std::endl;
+
+			// Register another base for player movment, this one is for reciving messages
+			ez.RegisterBase<PlayerMovment, MsgRecive<MoveMessage>>();
+
+			// Create a entity
+			enteez::Entity* entity = em.CreateEntity();
+
+			// Add the position and player movment components
+			entity->AddComponent<Position>(1.0f,2.0f,3.0f);
+			entity->AddComponent<PlayerMovment>(entity);
+
+			// Create a new message to send to the entity
+			MoveMessage msg{};
+			msg.x = 3.0f;
+			msg.y = 2.0f;
+			msg.z = 1.0f;
+
+			// Call the user defined function to send messages, with no sender set
+			SendMessage(entity,nullptr, msg);
+
+
+			// There is no inbuilt safeguard for when a component dose not exist for effitency, so it is down to the user to make sure entitys exist before accessing
+			if (entity->HasComponent<Position>())
 			{
-				std::cout << "b" << std::endl;
-			}, true);
-
-
-
-		delete position;
+				Position& position = entity->GetComponent<Position>();
+				std::cout << "Position: X:" << position.x << ", Y:" << position.y << ", Z:" << position.z << std::endl;
+			}
+			// Cleanup
+			entity->Destroy();
+		}
 
 	}
-
+	
+	// End of memory leak checking
 	_CrtDumpMemoryLeaks();
 
     return 0;
